90cdd19dd5eb9e73de496794a89c9dc4393ca180
Deswandy <deswandywong.23@gmail.com>
2025-12-23 18:22:10 +0700

    feat(ui): add shadcn

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,20 @@
+import { Button } from '@/components/ui/button'
 import { createFileRoute } from '@tanstack/react-router'
+import { useState } from 'react'
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  const [clicks, setClicks] = useState(0);
+
+
   return <div>
     <h1>The reason I made this boilerplate</h1>
     <p>I have done these steps mutliple times, for multiple projects, and I'm too lazy to repeat the same thing over and over and over again.</p>
+    <Button onClick={()=>{setClicks(clicks+1)}}>
+      Click me! {clicks}
+    </Button>
   </div>
 }
